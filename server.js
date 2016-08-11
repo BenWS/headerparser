f26836ac86659821bb41204918a4328b3487a4bd
Ben <shippeyben@gmail.com>
2016-08-11 02:13:41 +0000

    Project complete

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var http = require("http");
+var url = require('url');
+var app = express();
+
+app.get("/whoami", (req, res) => {
+
+    var myURL = url.parse(req.url, true);
+    var myPath = myURL.pathname;
+    
+    var results = req.headers; 
+    console.log(results);
+    
+    var opSystem = results['user-agent'].slice(results['user-agent'].indexOf("(") + 1, results['user-agent'].indexOf(")"));
+    var language = results['accept-language'].split(",")[0];
+    var ipAddress = results['x-forwarded-for'];
+    
+    res.json({"ipaddress":ipAddress,"language":language, "software":opSystem});
+    res.end();
+})
+
+app.get("/*", (req, res) => {
+    res.end("404!");
+})
+
+app.listen(8080);
